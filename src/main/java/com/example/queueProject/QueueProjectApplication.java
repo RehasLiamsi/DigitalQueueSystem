51e4fcbfc6ee5b9d14a0173e184903c7bf6a0c10
RehasLiamsi <nsaher91@gmail.com>
2023-11-23 15:06:33 +0100

    Initial commit

---
+++ src/main/java/com/example/queueProject/QueueProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.queueProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueProjectApplication.class, args);
+	}
+
+}
