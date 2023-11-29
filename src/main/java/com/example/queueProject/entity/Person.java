a86d79f0e1f699ae5505abc92d241cee8af417a4
RehasLiamsi <nsaher91@gmail.com>
2023-11-29 13:17:30 +0100

    Fix CRUD functionalities

--- src/main/java/com/example/queueProject/entity/Person.java
+++ src/main/java/com/example/queueProject/entity/Person.java
@@ -13,12 +13,19 @@ public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="person_id")
     private Long personId;
+
+    @Column(name="position_in_queue")
     Long positionInQueue;
+
+    @Column(name="joined_at_time")
     LocalDateTime joinedAtTime;
+
+    @Column(name="left_at_time")
     LocalDateTime leftAtTime;
 
     @ManyToOne
-    @JoinColumn(name="personQueueIdFK")
+    @JoinColumn(name="person_queue_idfk")
     private Queue queue;
 }
--- src/main/java/com/example/queueProject/entity/Queue.java
+++ src/main/java/com/example/queueProject/entity/Queue.java
@@ -4,16 +4,18 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter
 @Setter
 public class Queue {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="queue_id")
     private Long queueId;
+
+    @Column(name="queue_name")
     String queueName;
+
+    @Column(name="queue_status")
     Boolean queueStatus;
 }
