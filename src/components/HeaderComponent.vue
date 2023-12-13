3a551184cd08a51101e6de2f54564829079612a5
RehasLiamsi <nsaher91@gmail.com>
2023-12-13 15:10:10 +0100

    Style login page and delete some unwanted files

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template><div>
+<img src="../assets/HumanityFirst.jpg" alt="Humanity first logo">
+</div></template>
+
+<style scoped>
+img{
+  height: 4.5em;
+}
+</style>
