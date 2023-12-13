bd721cdfee2698fba760f7636f7448ad4688ce9e
RehasLiamsi <nsaher91@gmail.com>
2023-12-13 15:50:28 +0100

    Commit changes

--- src/main/java/com/example/queueProject/controller/AuthenticationController.java
+++ src/main/java/com/example/queueProject/controller/AuthenticationController.java
@@ -5,11 +5,9 @@ import com.example.queueProject.auth.UserRegisterOrAuthenticateRequest;
 import com.example.queueProject.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
