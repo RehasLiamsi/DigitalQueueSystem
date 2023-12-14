b99d10665036798faf2613f13dd6b87bd189bbfb
RehasLiamsi <nsaher91@gmail.com>
2023-12-14 14:29:18 +0100

    Fix CORS

--- src/main/java/com/example/queueProject/config/SecurityConfig.java
+++ src/main/java/com/example/queueProject/config/SecurityConfig.java
@@ -20,7 +20,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        http.
+                cors()
+                .and()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
