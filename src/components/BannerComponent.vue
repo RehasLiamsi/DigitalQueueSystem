7473f2cf3bd6b86312434a7739a3788e77070a8e
RehasLiamsi <nsaher91@gmail.com>
2023-12-19 16:35:03 +0100

    Shift banner to separate component

---
+++ src/components/BannerComponent.vue
@@ -0,0 +1,112 @@
+<script setup>
+import axiosInstance from "@/axiosConfig";
+import {onMounted, ref} from "vue";
+
+const showDropdown = ref(false)
+const activeQueueId = ref('');
+
+
+const fetchActiveQueueId = async () => {
+  try {
+    const response = await axiosInstance.get('queue/active/id')
+    activeQueueId.value = response.data;
+  } catch (error) {
+    console.error("There was an error fetching the active queue Id:", error);
+  }
+}
+
+const toggleQueueStatus = async () => {
+  try {
+    await axiosInstance.put(`queue/${activeQueueId.value}`);
+  } catch (error) {
+    console.error("There was an error toggling the queue status:", error);
+  }location.reload();
+}
+
+const toggleDropdown = () => {
+  showDropdown.value = !showDropdown.value
+}
+
+onMounted(fetchActiveQueueId);
+
+</script>
+<template>
+<div id="banner">
+<p>Welcome Admin!</p>
+<div class="router-links">
+  <div class="dropdown" @click="toggleDropdown">
+    <span class="dropdown-link">Settings</span>
+    <div v-if="showDropdown" class="dropdown-content">
+      <div  v-if="activeQueueId != ''"  @click="toggleQueueStatus">Close queue</div>
+      <div  v-else>
+        <RouterLink to="/queues">Queues</RouterLink></div>
+    </div>
+
+  </div>
+
+  <!--      <RouterLink to="/settings">Settings</RouterLink>-->
+  <!--      <RouterLink to="/statistics">Statistics</RouterLink>-->
+  <RouterLink to="/logout">Logout</RouterLink>
+
+
+</div>
+</div>
+</template>
+
+<style scoped>
+#banner{
+  background: #0167b2;
+  padding: 0.4em 4em;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.router-links{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.dropdown-link,
+.router-links a{
+  text-decoration: none;
+  color: white;
+  cursor: pointer;
+  margin: 10px;
+}
+
+.dropdown-link{
+  display: inline-flex;
+  margin-bottom: 0;
+}
+
+.dropdown-content {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  position: absolute;
+  background-color: #f9f9f9;
+  color: black;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  padding: 12px 0 12px 12px;
+  margin-top: 0.5em;
+  min-width: 120px;
+  cursor: pointer;
+}
+
+.dropdown-content ul{
+  list-style: none;
+  margin: 0;
+  padding: 0.5em 0 0.5em 1em;
+}
+
+.dropdown-content a{
+  padding: 0.5em 0.2em;
+  width: auto;
+  text-decoration: none;
+  color: black;
+
+}
+</style>
