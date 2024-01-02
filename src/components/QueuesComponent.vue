fa005d981c4545f6c5e1b948df4108e3c9599cbf
RehasLiamsi <nsaher91@gmail.com>
2024-01-02 11:09:45 +0100

    Print all queue names on screen

--- src/components/QueuesComponent.vue
+++ src/components/QueuesComponent.vue
@@ -1,50 +1,45 @@
 <script setup>
 import BannerComponent from "@/components/BannerComponent.vue";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import axiosInstance from "@/axiosConfig";
 
 const queues = ref('');
-
-const newQueue = ref('');
+// const newQueue = ref('');
+// const name = ref('');
+// const status = ref('');
+// const details = {name: name.value, status: status.value}
 
 const fetchAllQueues = async() => {
-
-}
-
-function addNewTask() {
-  if (newQueue.value.trim() === "") return;
-  tasks.value.push({ text: newQueue.value, completed: false });
-  newQueue.value = "";
-}
-
-const addNewQueue = async(details) => {
   try{
-    const response = await axiosInstance.post('queue');
-
+    const response = await axiosInstance.get('queue');
+    queues.value = response.data;
+  }catch (error){
+    console.log("Error fetching all queues:", error);
   }
 }
 
-function removeTask(task) {
-  tasks.value = tasks.value.filter((remove) => remove !== task);
-}
+// const addNewQueue = async(details) => {
+//   try{
+//     const response = await axiosInstance.post('queue');
+//     console.log(response.data);
+//   } catch (error) {
+//     console.log("Error adding new queue:", error);
+//   }
+// }
+onMounted(fetchAllQueues);
 </script>
 <template>
   <BannerComponent />
     <p>The following are the queues</p>
-  <div id="container">
-    <div id="queue-list">
       <ul>
-        <li v-for="queue in queues" :key="queue.text" :class="{ completed: queue.completed }">
-          <input type="checkbox" v-model="queue.completed" />
-          {{ queue.text }}
-          <button id="remove-button" @click="removeTask(queue)">Delete</button>
-        </li>
+        <li v-for="queue in queues" :key="queue">
+         {{queue.queueName}}</li>
       </ul>
-    </div>
-    <div id="add-new-task">
-      <textarea v-model="newQueue" placeholder="Add new task"></textarea>
-      <button id="add-button" @click="addNewTask">Add</button>
-    </div>
-  </div>
+
+<!--    <div id="add-new-task">-->
+<!--      <textarea v-model="newQueue" placeholder="Add new task"></textarea>-->
+<!--      <button id="add-button" @click="addNewTask">Add</button>-->
+<!--    </div>-->
+
 </template>
 <style scoped></style>
