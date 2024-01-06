e6a1044f0ee0ee05ad8b97419128066c7abf78c8
RehasLiamsi <nsaher91@gmail.com>
2024-01-06 18:10:42 +0100

    Start working on the user webpage

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template><div>
+<img src="../assets/HumanityFirst.jpg" alt="Humanity first logo">
+</div></template>
+
+<style scoped>
+img{
+  height: 4.5em;
+  margin-bottom: 0.8em;
+}
+</style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,6 +1,25 @@
 <script setup>
+
+import {onMounted, ref} from "vue";
+import axios from "axios";
+
+const person = ref('');
+
+const addPersonToQueue = async () => {
+  try {
+    const response = await axios.post('http://localhost:8080/person/add/2');
+    person.value = response.data;
+  } catch (error) {
+    console.error("Error adding person to queue:", error);
+  }
+}
+onMounted(addPersonToQueue);
 </script>
 
 <template>
-  <div></div>
+  <div class="text-box">
+    <p>Din plats i kön är <span> {{person.positionInQueue}}</span></p>
+    <p></p>
+  </div>
+  <img src="../assets/360_F_543201527_CKqnLxlnnMjYJVLHDk6l69gzQwSfVh8X.jpg" alt="spinning arrow" />
 </template>
