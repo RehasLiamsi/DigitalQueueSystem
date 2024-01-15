11c5aac36e5d120df88bdd0fe85d01e56584d0cb
RehasLiamsi <nsaher91@gmail.com>
2024-01-15 17:42:46 +0100

    First attempt at troubleshooting

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,18 +1,28 @@
 <script setup>
 import {onMounted, onUnmounted, ref} from "vue";
 import {useRouter} from "vue-router";
-import axios from "axios";
+import axiosInstance from "@/axiosConfig.js";
 
 const person = ref('');
 const hasJoinedQueue = ref(sessionStorage.getItem('joinedQueue') === 'true');
 const router = useRouter();
 const positionInQueue = ref('');
+const activeQueueId = ref('');
+const storedPerson = sessionStorage.getItem('person');
 
-
+const fetchActiveQueueId = async () => {
+  try {
+    const response = await axiosInstance.get(`/queue/active/id`)
+    activeQueueId.value = response.data;
+    console.log('Active queue id is:', activeQueueId.value)
+  } catch (error) {
+    console.error('Error fetching active queue id:', error);
+  }
+}
 const addPersonToQueue = async () => {
   if (!sessionStorage.getItem('joinedQueue')) {
     try {
-      const response = await axios.post('http://localhost:8080/person/add/1');
+      const response = await axiosInstance.post(`/person/add/${activeQueueId.value}`);
       person.value = response.data;
       sessionStorage.setItem('person', JSON.stringify(response.data))
       sessionStorage.setItem('joinedQueue', 'true');
@@ -28,7 +38,7 @@ const addPersonToQueue = async () => {
 
 const fetchQueuePosition = async () => {
   try {
-    const response = await axios.get(`http://localhost:8080/person/${person.value.personId}/position`);
+    const response = await axiosInstance.get(`/person/${person.value.personId}/position`);
     positionInQueue.value = response.data;
   } catch (error) {
     console.log("Person ID: ", person.value.personId)
@@ -39,7 +49,7 @@ const fetchQueuePosition = async () => {
 
 const leaveQueue = async () => {
   try {
-    await axios.put(`http://localhost:8080/person/left/${person.value.personId}`);
+    await axiosInstance.put(`/person/left/${person.value.personId}`);
     sessionStorage.removeItem('joinedQueue');
     sessionStorage.removeItem('person');
     person.value = {};
@@ -51,19 +61,23 @@ const leaveQueue = async () => {
 
 }
 
-onMounted(() => {
-  const storedPerson = sessionStorage.getItem('person');
+const conditionalAdditionOfPerson = () => {
   if (storedPerson) {
     console.log('Person already in session storage')
     person.value = JSON.parse(storedPerson);
-    sessionStorage.setItem('joinedQueue','true');
+    sessionStorage.setItem('joinedQueue', 'true');
     fetchQueuePosition();
   } else {
     addPersonToQueue();
   }
-  setInterval(fetchQueuePosition, 10000);
+}
 
-});
+const fetchData = async () =>{
+  await fetchActiveQueueId();
+  await conditionalAdditionOfPerson();
+}
+onMounted(fetchData);
+setInterval(fetchQueuePosition, 5000);
 onUnmounted(() => {
   clearInterval();
 })
@@ -72,9 +86,15 @@ onUnmounted(() => {
 
 <template>
   <div class="text-box">
-    <p v-if="hasJoinedQueue">Din plats i kön är <span> {{ positionInQueue }}</span></p>
-    <p v-else>Du måste ställa dig i kön genom att skanna QR coden</p>
-    <button @click="leaveQueue()">Hoppa av kön</button>
+    <div v-if ="hasJoinedQueue">
+    <p>Din plats i kön är <span> {{ positionInQueue }}</span></p>
+      <button @click="leaveQueue()">Hoppa av kön</button>
+    </div>
+    <div v-else>
+    <p>Tryck på knappen nedan för att ställa dig i kön</p>
+      <button @click="conditionalAdditionOfPerson()">Ställ dig i kön</button>
+<!--    <p>Din plats i kön är <span> {{ positionInQueue }}</span></p>-->
+  </div>
   </div>
   <img src="../assets/360_F_543201527_CKqnLxlnnMjYJVLHDk6l69gzQwSfVh8X.jpg" alt="spinning arrow"/>
 </template>
