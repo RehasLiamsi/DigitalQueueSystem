affb63a6ec1168dd5d5ae0eb0f5311f7b6a4a1c9
RehasLiamsi <nsaher91@gmail.com>
2024-01-16 11:34:17 +0100

    Troubleshoot

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,41 +8,43 @@ const hasJoinedQueue = ref(sessionStorage.getItem('joinedQueue') === 'true');
 const router = useRouter();
 const positionInQueue = ref('');
 const activeQueueId = ref('');
-const storedPerson = sessionStorage.getItem('person');
+
 
 const fetchActiveQueueId = async () => {
   try {
     const response = await axiosInstance.get(`/queue/active/id`)
     activeQueueId.value = response.data;
-    console.log('Active queue id is:', activeQueueId.value)
+    console.log('(fetchActiveQueueId) Active queue id is:', activeQueueId.value)
   } catch (error) {
-    console.error('Error fetching active queue id:', error);
+    console.error('(fetchActiveQueueId) Error fetching active queue id:', error);
   }
 }
 const addPersonToQueue = async () => {
-  if (!sessionStorage.getItem('joinedQueue')) {
+  //if (!sessionStorage.getItem('joinedQueue')) {
     try {
       const response = await axiosInstance.post(`/person/add/${activeQueueId.value}`);
       person.value = response.data;
       sessionStorage.setItem('person', JSON.stringify(response.data))
       sessionStorage.setItem('joinedQueue', 'true');
+      console.log("(addPersonToQueue) Person added to queue")
+      await fetchQueuePosition();
     } catch (error) {
-      console.error("Error adding person to queue:", error);
+      console.error("(addPersonToQueue) Error adding person to queue:", error);
     }
-  } else {
-    console.log('Already joined the queue', person.value.personId)
-    const storedPerson = sessionStorage.getItem('person');
-    person.value = JSON.parse(storedPerson);
-  }
-}
+  } //else {
+    //console.log('Already joined the queue', person.value.personId)
+    //const storedPerson = sessionStorage.getItem('person');
+    //person.value = JSON.parse(storedPerson);
+  //}
+//
 
 const fetchQueuePosition = async () => {
   try {
     const response = await axiosInstance.get(`/person/${person.value.personId}/position`);
     positionInQueue.value = response.data;
   } catch (error) {
-    console.log("Person ID: ", person.value.personId)
-    console.error("Error fetching queue position:", error);
+    console.log("(fetchQueuePosition) Person ID: ", person.value.personId)
+    console.error("(fetchQueuePosition) Error fetching queue position:", error);
   }
 }
 
@@ -56,18 +58,20 @@ const leaveQueue = async () => {
     clearInterval();
     await router.push('/leftQueue')
   } catch (error) {
-    console.error("Error leaving the queue:", error);
+    console.error("(leaveQueue) Error leaving the queue:", error);
   }
 
 }
 
 const conditionalAdditionOfPerson = () => {
+  const storedPerson = sessionStorage.getItem('person');
   if (storedPerson) {
-    console.log('Person already in session storage')
+    console.log('(conditionalAdditionOfPerson) Person already in session storage')
     person.value = JSON.parse(storedPerson);
     sessionStorage.setItem('joinedQueue', 'true');
     fetchQueuePosition();
   } else {
+    console.log("(conditionalAdditionOfPerson) Adding person to queue")
     addPersonToQueue();
   }
 }
@@ -75,9 +79,9 @@ const conditionalAdditionOfPerson = () => {
 const fetchData = async () =>{
   await fetchActiveQueueId();
   await conditionalAdditionOfPerson();
+  setInterval(fetchQueuePosition, 5000);
 }
 onMounted(fetchData);
-setInterval(fetchQueuePosition, 5000);
 onUnmounted(() => {
   clearInterval();
 })
@@ -86,15 +90,15 @@ onUnmounted(() => {
 
 <template>
   <div class="text-box">
-    <div v-if ="hasJoinedQueue">
+<!--    <div v-if ="hasJoinedQueue">-->
     <p>Din plats i kön är <span> {{ positionInQueue }}</span></p>
       <button @click="leaveQueue()">Hoppa av kön</button>
-    </div>
-    <div v-else>
-    <p>Tryck på knappen nedan för att ställa dig i kön</p>
-      <button @click="conditionalAdditionOfPerson()">Ställ dig i kön</button>
-<!--    <p>Din plats i kön är <span> {{ positionInQueue }}</span></p>-->
-  </div>
+<!--    </div>-->
+<!--    <div v-else>-->
+<!--    <p>Tryck på knappen nedan för att ställa dig i kön</p>-->
+<!--      <button @click="conditionalAdditionOfPerson()">Ställ dig i kön</button>-->
+<!--   <p>Din plats i kön är <span> {{ positionInQueue }}</span></p>-->
+<!--  </div>-->
   </div>
   <img src="../assets/360_F_543201527_CKqnLxlnnMjYJVLHDk6l69gzQwSfVh8X.jpg" alt="spinning arrow"/>
 </template>
