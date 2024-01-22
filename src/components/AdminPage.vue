1614f060e2f95bb19c78614d6b47339e44a86f8c
RehasLiamsi <nsaher91@gmail.com>
2024-01-22 14:12:12 +0100

    Fix functionality of closing queue so that it still shows how many people are standing in a closed queue

--- src/components/AdminPage.vue
+++ src/components/AdminPage.vue
@@ -6,13 +6,14 @@ import BannerComponent from "@/components/BannerComponent.vue";
 
 const peopleCount = ref('');
 const activeQueueName = ref('');
-const activeQueueId = ref('');
+const fetchedActiveQueueId = ref('');
+const storedActiveQueueId = ref('');
 const totalPeopleServed = ref('');
 const firstPersonInQueue = ref('');
 const buttonText = ref('Close Queue');
 
 const fetchPeopleCount = async () => {
-  if (activeQueueId.value) {
+  if (fetchedActiveQueueId.value) {
     const response = await axiosInstance.get('/person/count/activeQueue');
     peopleCount.value = response.data;
   }
@@ -25,11 +26,13 @@ const fetchActiveQueueName = async () => {
 
 const fetchActiveQueueId = async () => {
   const response = await axiosInstance.get('/queue/active/id');
-  activeQueueId.value = response.data;
+  fetchedActiveQueueId.value = response.data;
+  sessionStorage.setItem('storedActiveQueueId', response.data);
 }
 
 const fetchTotalPeopleServedToday = async () => {
-  const response = await axiosInstance.get(`/person/count/all/${activeQueueId.value}`);
+  storedActiveQueueId.value = sessionStorage.getItem('storedActiveQueueId')
+  const response = await axiosInstance.get(`/person/count/all/${storedActiveQueueId.value}`);
   totalPeopleServed.value = response.data;
 }
 
@@ -49,23 +52,25 @@ const dropFirstPersonInQueue = async () => {
 }
 
 const closeQueue = async () => {
-  buttonText.value = buttonText.value === 'Close Queue' ? 'Open Queue' : 'Close Queue'
   try {
-    await axiosInstance.put(`queue/${activeQueueId.value}`);
+    await axiosInstance.put(`queue/${fetchedActiveQueueId.value}`);
+    buttonText.value = buttonText.value === 'Close Queue' ? 'Open Queue' : 'Close Queue'
+    fetchedActiveQueueId.value = '';
   } catch (error) {
     alert("OOPS! Couldn't close queue!")
   }
 }
 
 const fetchDataOnMounted = async () => {
-  await fetchActiveQueueName();
+  await fetchActiveQueueId();
   await fetchAllData();
 }
 
 const fetchAllData = async () => {
-  await fetchActiveQueueId();
-  if (activeQueueId.value) {
+  storedActiveQueueId.value = sessionStorage.getItem('storedActiveQueueId')
+  if (fetchedActiveQueueId.value) {
     await fetchPeopleCount();
+    await fetchActiveQueueName();
   }
   await fetchTotalPeopleServedToday();
 }
